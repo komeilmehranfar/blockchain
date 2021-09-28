6bdbb34fb71a06d245bffa0cac018cd795970960
komeilmehranfar <komeil.mehranfar@gmail.com>
2021-09-28 22:27:18 +0330

    add createNewBlock method

--- src/blockchain.js
+++ src/blockchain.js
@@ -2,3 +2,21 @@ function Blockchain() {
   this.chain = [];
   this.newTransactions = [];
 }
+
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const block = {
+    nonce,
+    previousBlockHash,
+    hash,
+    index: this.chain.length + 1,
+    transactions: this.newTransactions,
+    timestamp: Date.now(),
+  };
+  this.newTransactions = [];
+  this.chain.push(block);
+  return block;
+};
